5c2ec9d7d7f9e52de7d0f07b068316205fa92604
ayushmaster100@gmail.com <ayushmaster100@gmail.com>
2023-02-22 23:24:49 +0530

    Added Vercel Config

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import express from "express";
 import fetch from "node-fetch";
 const app = express();
-app.listen(3000, () => console.log("Server started"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started"));
 app.get("/", (req, res) => {
   fetch("https://streak-stats.demolab.com/?user=weryzebra-yue&type=json")
     .then((response) => response.json())
